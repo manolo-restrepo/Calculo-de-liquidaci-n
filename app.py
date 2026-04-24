25737d26d623d16f66761b524e4e0ac6e75b36d3
ADOPCIÓN Y VENTA DE MASCOTAS <ospinazabalamariapaula@gmail.com>
2026-04-24 11:54:09 -0500

    se eliminan archivos innecesarios

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import sys
-sys.path.append( "src" )
+import sys 
+import os 
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from view_gui.payment_gui import LiquidacionApp
 
-LiquidacionApp().run()
+if __name__ == "__main__":
+    LiquidacionApp().run()
